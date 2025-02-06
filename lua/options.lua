e783e6c9fcab72ec3f6b8763511f61fdb0d23c9c
tavo <tavo.lop33@gmail.com>
2025-02-06 09:34:49 -0300

    clangd y harpoon
    quito clangd temporalmente, agrego harpoon

--- lua/options.lua
+++ lua/options.lua
@@ -6,8 +6,8 @@ local o = vim.o
 o.cursorlineopt = "both" -- to enable cursorline!
 
 -- folds
-vim.o.foldcolumn = "1" -- '0' is not bad
-vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-vim.o.foldlevelstart = 99
-vim.o.foldenable = true
-vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+o.foldcolumn = "1" -- '0' is not bad
+o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+o.foldlevelstart = 99
+o.foldenable = true
+o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
