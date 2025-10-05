63fdf18de0133bb8293f43376b0585be1a1407e4
tavo <tavo.lop33@gmail.com>
2025-10-05 19:51:21 -0300

    agrego lsp de lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -309,29 +309,4 @@ return {
     version = "^6", -- Recommended
     lazy = false, -- This plugin is already lazy
   },
-
-  -- arduino/esp32
-  {
-    "anurag3301/nvim-platformio.lua",
-    dependencies = {
-      { "akinsho/nvim-toggleterm.lua" },
-      { "nvim-telescope/telescope.nvim" },
-      { "nvim-lua/plenary.nvim" },
-    },
-    cmd = {
-      "Pioinit",
-      "Piorun",
-      "Piocmd",
-      "Piolib",
-      "Piomon",
-      "Piodebug",
-      "Piodb",
-    },
-    config = function()
-      require("platformio").setup {
-        lsp = "ccls", --default: ccls, other option: clangd
-        -- If you pick clangd, it also creates compile_commands.json
-      }
-    end,
-  },
 }
